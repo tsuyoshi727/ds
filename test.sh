beacd9a2ba304df43487079df354ee1b8da2ced6
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-30 15:05:14 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,18 +2,13 @@
 echo "设置时区"
 sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-git branch -r
-ls
+chmod 755 ./*.sh
+# echo "拉取源码"
+# git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
+cp -f * ~/scripts/
+cd ~/scripts/
 echo "修改源码"
 sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
 sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
 npm install
-
-echo "拉取源码"
-#git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
-mkdir ~/scripts
-cp -rf ./*  ~/scripts
-
-cd ..
-[ -e "run_scripts.sh" ] && chmod 755 run_scripts.sh
+chmod 755 ./*.sh

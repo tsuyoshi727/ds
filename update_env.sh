f248cbed85acfe09df632d161744a77075cb4a31
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-30 10:37:42 +0800

    Update update_env.sh

--- update_env.sh
+++ update_env.sh
@@ -4,7 +4,7 @@ sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 echo "拉取源码"
-#git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
+git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
 echo "修改源码"
 sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
 sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`

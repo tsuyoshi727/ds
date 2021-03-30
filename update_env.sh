73d6b17a380d77e3484a87152759b7d360f25ad7
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-30 10:09:50 +0800

    Update update_env.sh

--- update_env.sh
+++ update_env.sh
@@ -7,11 +7,13 @@ git checkout -b jd
 
 echo "拉取源码"
 #git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
+mkdir ~/scripts
 cp -rf ./* cd  ~/scripts
 cd  ~/scripts
 echo "修改源码"
 sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
 sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
-
 npm install
+
+cd ..
 [ -e "run_scripts.sh" ] && chmod 755 run_scripts.sh

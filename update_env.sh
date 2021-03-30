5c717cb9345596d017663d2e663ef5ca2d03e02f
tracefish <64358864+tracefish@users.noreply.github.com>
2021-03-30 15:22:16 +0800

    Update update_env.sh

--- update_env.sh
+++ update_env.sh
@@ -2,10 +2,14 @@
 echo "设置时区"
 sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-echo "拉取源码"
-git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
+chmod 755 ./*.sh
+# echo "拉取源码"
+# git clone --depth 1 -b jd https://github.com/tracefish/ds.git ~/scripts
+mkdir ~/scripts/
+cp -rf * ~/scripts/
+cd ~/scripts/
 echo "修改源码"
 sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
 sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
 npm install
+chmod 755 ./*.sh
